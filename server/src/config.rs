a478b3c19e6408c429182476a9523c30d0dcfc03
Vanilla Viking <ashwinr2k2@gmail.com>
2025-07-08 18:51:30 +1000

    feat: server cli args

---
+++ server/src/config.rs
@@ -0,0 +1,68 @@
+use std::{env::{self}, path::PathBuf, process::exit};
+
+use log::{error, LevelFilter};
+
+#[derive(Debug)]
+pub struct Config {
+    pub port: u16,
+    pub log_level: LevelFilter,
+    pub store_size: u32,
+    pub db_file: PathBuf,
+}
+
+impl Config {
+    pub fn default() -> Self {
+        Self { port: 7884, log_level: LevelFilter::Info, store_size: 200, db_file: PathBuf::from("pastebuff.db") }
+    }
+}
+
+
+pub fn parse_args() -> Config {
+    let args: Vec<String> = env::args().collect();
+    let mut config = Config::default();
+    let mut n = 0;
+    while n < args.len()-1 {
+        match args[n].as_str() {
+            "-p" | "--port" => {
+                if let Ok(p) = args[n+1].parse::<u16>() {
+                    config.port = p;
+                    n+=1;
+                } else {
+                    error!("Invalid port argument");
+                    exit(1);
+                }
+            },
+            "-l" | "--log-level" => {
+                let log_level = match args[n+1].as_str() {
+                    "trace" => LevelFilter::Trace,
+                    "debug" => LevelFilter::Debug,
+                    "info" => LevelFilter::Info,
+                    "warn" => LevelFilter::Warn,
+                    "error" => LevelFilter::Error,
+                    _ => {
+                        error!("Invalid log level argument");
+                        exit(1);
+                    }
+                };
+                config.log_level = log_level;
+                n+=1
+            },
+            "-s" | "--store-size" => {
+                if let Ok(s) = args[n+1].parse::<u32>() {
+                    config.store_size = s;
+                    n+=1;
+                } else {
+                    error!("Invalid store size argument");
+                    exit(1);
+                }
+            },
+            "--db" => {
+                config.db_file = PathBuf::from(args[n+1].clone());
+            }
+            _ => ()
+        }
+        n+=1
+    }
+
+    config
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,19 @@
 use log::{debug, error, info, trace, warn, LevelFilter};
 use tokio::{io::{AsyncBufReadExt, BufReader}, net::{TcpListener, TcpStream}};
 
+mod config;
+use config::parse_args;
+
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7884").await.unwrap();
-    colog::basic_builder().filter(None, LevelFilter::Debug).init();
+    let config = parse_args();
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", config.port)).await.unwrap();
+    colog::basic_builder().filter(None, config.log_level).init();
     
-    info!("Server listening on port 7884");
+    info!("Server listening on port {}", config.port);
     loop {
         let (socket, _) = listener.accept().await.unwrap();
+        let _ =socket.peer_addr().inspect(|addr| info!("Connection established to {}", addr));
         // A new task is spawned for each inbound socket. The socket is
         // moved to the new task and processed there.
         tokio::spawn(async move {
@@ -19,10 +24,22 @@ async fn main() {
 
 async fn handle_request(socket: TcpStream) {
     let mut buf_reader = BufReader::new(socket);
-    let mut req = String::new();
-    match buf_reader.read_line(&mut req).await {
-        Ok(n) => debug!("Read {} bytes from client request", n),
-        Err(e) => error!("Could not read from stream: {}", e),
+    loop {
+        let mut req = String::new();
+        match buf_reader.read_line(&mut req).await {
+            Ok(0) => {
+                if req.is_empty() {
+                    // since buffer is empty, it means client has closed the connection
+                    let _ = buf_reader.into_inner().peer_addr().inspect(|addr| info!("Socket closed by client {}", addr));
+                    break;
+                } else {
+                    error!("Connection reset by peer");
+                    break;
+                }
+            },
+            Ok(n) => debug!("Read {} bytes from client request", n),
+            Err(e) => error!("Could not read from stream: {}", e),
+        }
+        debug!("Got request string: {}", req);
     }
-    debug!("Got request string: {}", req);
 }
