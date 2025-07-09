dc940c6d61eead2d7ecc54d7010b36f33fccf5d7
Vanilla Viking <ashwinr2k2@gmail.com>
2025-07-09 23:03:40 +1000

    wipfeat: store

--- server/src/store.rs
+++ server/src/store.rs
@@ -1,2 +1,18 @@
 
+struct KVStore<T: Clone> {
+    buffer: Vec<Option<Selection<T>>>,
+    capacity: usize,
+}
 
+#[derive(Debug, Clone)]
+struct Selection<T: Clone> {
+    value: T,
+    next: usize,
+    prev: usize,
+}
+
+impl<T: Clone> KVStore<T> {
+    fn new(capacity: usize) -> Self {
+        KVStore { buffer: vec![None; capacity], capacity } 
+    }
+}
